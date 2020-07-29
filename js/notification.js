7df9f1845f04cad6e907126cc0b9134341bd94d7
joann20 <annaminjee@hotmail.com>
2020-07-28 17:01:39 -0700

    json file addition and small changes

--- js/notification.js
+++ js/notification.js
@@ -1,36 +1,5 @@
 
 //allows for 5 minute timer for the duration of stretch break
-function startTimer(duration, display) {
-	let timer = duration;
-	let minutes;
-	let seconds;
-
-	// this only works if the window is open
-	setInterval(function () {
-		// converts to minute & seconds int in main number system (10)
-		minutes = parseInt(timer / 60, 10);
-		seconds = parseInt(timer % 60, 10);
-
-		//add 0 styling
-		minutes = minutes < 10 ? '0' + minutes : minutes;
-		seconds = seconds < 10 ? '0' + seconds : seconds;
-
-		// set display for html
-		display.textContent = minutes + ':' + seconds;
-
-		// if done, reset
-		if (--timer < 0) {
-			timer = 0;
-		}
-	}, 1000);
-}
-
-window.onload = function () {
-	let fiveMinute = 60 * 5,
-	display = document.querySelector('#timer'); //added this 'timer' id on notification.html
-	startTimer(fiveMinute, display);
-};
-
 navigator.permissions.query({name: 'microphone'})
  .then((permissionObj) => {
   console.log(permissionObj.state);
--- js/script.js
+++ js/script.js
@@ -1,40 +1,81 @@
-//used in glitch for webcam and model training
+//*********************NOTIFICATION PAGE*********************
 
-let video;
-let label = "check your form...";
-let classifier;
-let modelURL = 'https://storage.googleapis.com/tm-models/YadBJmj5/';
+function startTimer(duration, display) {
+	let timer = duration;
+	let minutes;
+	let seconds;
 
-// STEP 1: Load the model!
-function preload() {
-  classifier = ml5.imageClassifier(modelURL + 'model.json');
-}
+	// this only works if the window is open
+	setInterval(function () {
+		// converts to minute & seconds int in main number system (10)
+		minutes = parseInt(timer / 60, 10);
+		seconds = parseInt(timer % 60, 10);
 
-function setup() {
-  let c = createCanvas(640, 520);
-  c.position(8.75*width/10, height/8);
-  // Create the video
-  video = createCapture(VIDEO);
-  video.hide();
-}
+		//add 0 styling
+		minutes = minutes < 10 ? '0' + minutes : minutes;
+		seconds = seconds < 10 ? '0' + seconds : seconds;
 
+		// set display for html
+		display.textContent = minutes + ':' + seconds;
 
-function draw() {
-  background(0);
-
-  // Draw the video
-  image(video, 0, 0);
-
-  // STEP 4: Draw the label
-  textSize(32);
-  textAlign(CENTER, CENTER);
-  fill(255);
-  text(label, width / 2, height - 16);
-
+		// if done, reset
+		if (--timer < 0) {
+			timer = 0;
+		}
+	}, 1000);
 }
 
 window.onload = function () {
 	let fiveMinute = 60 * 5,
 	display = document.querySelector('#timer'); //added this 'timer' id on notification.html
-	// startTimer(fiveMinute, display);
+	startTimer(fiveMinute, display);
 };
+
+
+
+//*********************TEACHABLE MACHINE CODE IN NOTIFICATION PAGE*********************
+
+
+
+//*********************OLD TEACHABLE MACHINE CODE WITH P5*********************
+
+// // Teachable Machine
+// // The Coding Train / Daniel Shiffman
+// // https://thecodingtrain.com/TeachableMachine/1-teachable-machine.html
+// // https://editor.p5js.org/codingtrain/sketches/PoZXqbu4v
+
+// // The video
+// let video;
+// // For displaying the label
+// let label = "check your form...";
+// // The classifier
+// let classifier;
+// let modelURL = 'https://storage.googleapis.com/tm-models/YadBJmj5/';
+
+// // STEP 1: Load the model!
+// function preload() {
+//   classifier = ml5.imageClassifier(modelURL + 'model.json');
+// }
+
+// function setup() {
+//   let c = createCanvas(640, 520);
+//   c.position(8.75*width/10, height/8);
+//   // Create the video
+//   video = createCapture(VIDEO);
+//   video.hide();
+// }
+
+
+// function draw() {
+//   background(0);
+
+//   // Draw the video
+//   image(video, 0, 0);
+
+//   // STEP 4: Draw the label
+//   textSize(32);
+//   textAlign(CENTER, CENTER);
+//   fill(255);
+//   text(label, width / 2, height - 16);
+
+// }
