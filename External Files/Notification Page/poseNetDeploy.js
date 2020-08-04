4f0908ed7ec9bb5023064a55b1ebceb2a6b6e659
joann20 <annaminjee@hotmail.com>
2020-08-03 20:20:22 -0700

    clean up posenet code

--- External Files/Notification Page/poseNetDeploy.js
+++ External Files/Notification Page/poseNetDeploy.js
@@ -62,42 +62,43 @@ function classifyPose() {
 function gotResult(error, results) {
   var imagekey = localStorage.getItem("imagekey");
   console.log(imagekey)
-  switch(imagekey) {
-    case "1":
+  // switch(imagekey) {
+  //   case "1":
       
-      break;
-    case "2":
+  //     break;
+  //   case "2":
       
-      break;
-    case "3":
-      break;
-    case "4":
-      break;
-    case "5":
-      break;
-    case "6":
+  //     break;
+  //   case "3":
+  //     break;
+  //   case "4":
+  //     break;
+  //   case "5":
+  //     break;
+  //   case "6":
       
-      break;
-    case "7":
-      break;
-    case "8":
-      break;
-    case "9":
-      break;
-    case "10":
+  //     break;
+  //   case "7":
+  //     break;
+  //   case "8":
+  //     break;
+  //   case "9":
+  //     break;
+  //   case "10":
       
-      break;
-    case "11":
-      break;
-    case "12":
-      break;
-    case "13":
-      break;
-    case "14":
-      break;
-    case "15":
-      break;
-  }
+  //     break;
+  //   case "11":
+  //     break;
+  //   case "12":
+  //     break;
+  //   case "13":
+  //     break;
+  //   case "14":
+  //     break;
+  //   case "15":
+  //     break;
+  // }
+
   // if (results[0].label == "Shift") {
     // console.log(results[0].confidence);
   // }
